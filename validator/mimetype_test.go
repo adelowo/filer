818e766486d6e4b59ac34201c335dad772152ab5
Lanre Adelowo <me@lanreadelowo.com>
2017-05-17 04:20:19 +0100

    More tests to cover all cases

--- validator/mimetype_test.go
+++ validator/mimetype_test.go
@@ -12,6 +12,7 @@ import (
 var _ = Describe("Mimetype", func() {
 
 	var val Validator
+	var m *multipart.FileHeader
 
 	BeforeEach(func() {
 		val = NewMimeTypeValidator([]string{"image/jpeg", "image/png"})
@@ -23,6 +24,36 @@ var _ = Describe("Mimetype", func() {
 			isValid, err := val.Validate(&multipart.FileHeader{Filename: "unknown-file"})
 
 			Expect(err).To(HaveOccurred())
+			Expect(isValid).To(BeFalse())
+		})
+
+	})
+
+	Context("When validating a file with a known path", func() {
+
+		BeforeEach(func() {
+			m = &multipart.FileHeader{Filename: "./fixtures/gopher.jpg"}
+		})
+
+		It("should not have an error if the mimetype is valid", func() {
+			isValid, err := val.Validate(m)
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(isValid).To(BeTrue())
+		})
+
+		It("should have a truthy value if the mimetype is valid", func() {
+			Expect(val.Validate(m)).To(BeTrue())
+		})
+
+		It("should return an errror if the mimetype is invalid", func() {
+			val = NewMimeTypeValidator([]string{"application/octet-stream"})
+
+			isValid, err := val.Validate(m)
+
+			Expect(err).To(HaveOccurred())
+			Expect(err).Should(Equal(ErrFileInvalidMimeType))
+
 			Expect(isValid).To(BeFalse())
 		})
 
