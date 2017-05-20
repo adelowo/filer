6a43adea2bde30a7853743feb676c3a729251e92
Lanre Adelowo <me@lanreadelowo.com>
2017-05-20 02:28:58 +0100

    Refactored function name

--- validator/extension.go
+++ validator/extension.go
@@ -47,14 +47,14 @@ func isValidExtension(allowed []string, current string) bool {
 }
 
 func getExtensionFromFileName(fileName string) string {
-	return cleanExtension(path.Ext(fileName)[1:])
+	return normalizeExtension(path.Ext(fileName)[1:])
 }
 
 // This is here so as to remove all non-aphabetic characters.
 //The reasoning behind this is the fact that files are saved in the temp dir of the
 //system and Go suffixes them with some weird integer hence path.Ext would return
 //the integer alongside the original extension
-func cleanExtension(s string) string {
+func normalizeExtension(s string) string {
 
 	return strings.Map(func(r rune) rune {
 
